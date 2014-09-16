8885baed046d6fe6cb638105e628ad1b38097417
Joelromerog.11@gmail.com <Joelromerog.11@gmail.com@d1b6418f-b0fd-1ff2-78a8-cc738f0f65a5>
2014-09-16 02:39:44 +0000

    Subiendo Progra x.x

---
+++ trunk/Web_SGRESAN/src/java/bean/LoginBean.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bean;
+
+import dao.PersonaDao;
+import dao.UsuarioDao;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import model.TPerfil;
+import model.TPersona;
+import model.TUbigeo;
+import model.TUsuario;
+import org.primefaces.context.RequestContext;
+
+/**
+ *
+ * @author Joel
+ */
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+
+   
+    public TUsuario usuario;
+    public TPersona persona;
+    /**
+     * Creates a new instance of LoginBean
+     */
+    public LoginBean() {
+        usuario= new TUsuario();
+        usuario.setTPerfil(new TPerfil());
+        
+        persona = new TPersona();
+        persona.setTUsuario(new TUsuario());
+        persona.setTUbigeo(new TUbigeo());
+    }
+    
+     public void logueo()
+    {
+        RequestContext context = RequestContext.getCurrentInstance();  
+        FacesMessage msg = null;  
+        boolean loggedIn = false;  
+        UsuarioDao dao = new UsuarioDao();
+        PersonaDao pers = new PersonaDao();
+        usuario= dao.buscarporusuario(usuario);
+         
+        
+        if(usuario != null)
+        {
+            loggedIn = true;  
+            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", usuario.getNombreUsuario());  
+             persona = pers.BuscaporId(usuario.getNombreUsuario());
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuario", usuario);
+        }else
+        {
+            loggedIn = false; 
+              msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error", "Invalid credentials"); 
+              System.out.println("Falla");
+        }
+        FacesContext.getCurrentInstance().addMessage(null, msg); 
+        context.addCallbackParam("loggedIn", loggedIn);  
+    }
+
+     
+    public String logoud() {
+
+        RequestContext context = RequestContext.getCurrentInstance();
+        FacesContext fc = FacesContext.getCurrentInstance();
+        HttpSession sesion = (HttpSession) fc.getExternalContext().getSession(false);
+        sesion.invalidate();
+        //usuario = new TUsuario();
+        System.out.println(usuario.getNombreUsuario().length());
+        return "index.xhtml";
+        //
+    }
+     
+     public void PRUEBA()
+     {
+         System.out.println(usuario.getNombreUsuario().length());
+     }
+     
+     
+    public TUsuario getUsuario() {
+        if(usuario==null)
+        {
+            usuario = new TUsuario();
+        }
+        return usuario;
+    }
+
+    public void setUsuario(TUsuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public TPersona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(TPersona persona) {
+        this.persona = persona;
+    }
+    
+     
+     
+    
+}
