609eda87cd7b29764b1c7ff11617ff0dda96889b
caezar1 <caezar1@gmail.com>
2014-11-20 22:56:17 +0000

    quitar "javax.faces.resource/" de la url de las imagenes del switcher, por eso no se muestran

---
+++ Web_SGRESAN/src/java/bean/ImagesView.java
@@ -0,0 +1,30 @@
+package bean;
+ 
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.render.FacesRenderer;
+ 
+@ManagedBean
+public class ImagesView {
+     
+    private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 2; i++) {
+            images.add("banner_querencia_" + i + ".png");
+        }
+    }
+ 
+    public List<String> getImages() {
+        /*
+        String pattern = "javax.faces.resource";
+        String salida;
+        salida.replaceAll(pattern, );*/
+        System.out.print("get images 1: "+ images +" fin images1");
+        return images;
+    }
+}
