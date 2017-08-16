1b3f19ce2bf7312ab35ac6589f1529e5a59dea74
david <david@DavidPC>
2017-08-15 21:53:39 -0500

    15/08/2017 Formato de imagen

--- Web_SGRESAN/src/java/bean/ProductoBean.java
+++ Web_SGRESAN/src/java/bean/ProductoBean.java
@@ -14,6 +14,7 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import model.TCategoria;
 import model.TProducto;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.DefaultStreamedContent;
@@ -29,21 +30,21 @@ import org.primefaces.model.UploadedFile;
 public class ProductoBean {
 
     private TProducto producto;
-    
+
     private List<TCategoria> listaCategorias;
     private List<TProducto> listaProductos;
-    
-    ProductoDao pDao ;
+
+    ProductoDao pDao;
     private UploadedFile file;
     private StreamedContent imagen;
-    
+
     public ProductoBean() {
         producto = new TProducto();
         producto.setTCategoria(new TCategoria());
         pDao = new ProductoDao();
     }
-    
-    public void Inicializar(){
+
+    public void Inicializar() {
         producto = new TProducto();
         producto.setTCategoria(new TCategoria());
     }
@@ -51,17 +52,17 @@ public class ProductoBean {
     public void handleFileUpload(FileUploadEvent event) {
         FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
         FacesContext.getCurrentInstance().addMessage(null, message);
-        System.out.println("FILE :: "+event.getFile().getFileName());
-        try{
+        System.out.println("FILE :: " + event.getFile().getFileName());
+        try {
             byte[] bytes = IOUtils.toByteArray(event.getFile().getInputstream());
-               producto.setImagen(bytes);
-               imagen =new DefaultStreamedContent(new ByteArrayInputStream(producto.getImagen()));
-        }catch(Exception e){
-            System.out.println("Error :: "+e.getMessage());
+            producto.setImagen(Base64.encodeBase64String(bytes));
+            imagen = new DefaultStreamedContent(new ByteArrayInputStream(bytes));
+        } catch (Exception e) {
+            System.out.println("Error :: " + e.getMessage());
         }
     }
-    
-    public void SUBIR_IMAGEN(FileUploadEvent event){
+
+    public void SUBIR_IMAGEN(FileUploadEvent event) {
         System.out.println("PRUEBA");
         //System.out.println("ARCHIVO : "+event.getFile().getFileName() );
         /*try{
@@ -84,80 +85,68 @@ public class ProductoBean {
             System.out.println("ERROR : "+e.getMessage());
         }*/
     }
-    public void AGREGARPRODUCTO(){
- 
-       try
-        {
-            System.out.println("FILE : "+file.getContentType());
-            if(file.getContentType().substring(0, 1).equals("i"))
-           {
-      
-         
-              
-               byte[] bytes = IOUtils.toByteArray(file.getInputstream());
-               producto.setImagen(bytes);
-               System.out.println(file.getFileName());
-                       pDao.InsetartProducto(producto);
-        FacesContext context = FacesContext.getCurrentInstance();
-        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Exito", "Se agrego el producto "+ producto.getNombreProducto()));
-        
-                       
-           }else
-           {
-               FacesContext context = FacesContext.getCurrentInstance();
-              context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,"Advertencia",  "El Archivo que subio no es una imagen") );
-      
-           }
-        }
-        catch(Exception e){
-            System.out.println("ERROR : "+e.getMessage());
-        } 
 
-               
-        Inicializar(); 
+    public void AGREGARPRODUCTO() {
+
+        try {
+            System.out.println("FILE : " + file.getContentType());
+            if (file.getContentType().substring(0, 1).equals("i")) {
+
+                byte[] bytes = IOUtils.toByteArray(file.getInputstream());
+                String strBase64 = Base64.encodeBase64String(bytes);
+                producto.setImagen(strBase64);
+                System.out.println(file.getFileName());
+                pDao.InsetartProducto(producto);
+                FacesContext context = FacesContext.getCurrentInstance();
+                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Exito", "Se agrego el producto " + producto.getNombreProducto()));
+
+            } else {
+                FacesContext context = FacesContext.getCurrentInstance();
+                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Advertencia", "El Archivo que subio no es una imagen"));
+
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR : " + e.getMessage());
+        }
+
+        Inicializar();
     }
-    
-    public void ACTUALIZARPRODUCTO(){
+
+    public void ACTUALIZARPRODUCTO() {
         pDao.ActualizarProducto(producto);
         FacesContext context = FacesContext.getCurrentInstance();
-         context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Exito", "Se actualizo los datos del producto "+ producto.getNombreProducto()));
-        
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Exito", "Se actualizo los datos del producto " + producto.getNombreProducto()));
+
         Inicializar();
     }
-    
-    public void ELIMINARPRODUCTO(int id){
+
+    public void ELIMINARPRODUCTO(int id) {
         BUSCARPRODUCTO(id);
         pDao.EliminarProducto(producto);
-         FacesContext context = FacesContext.getCurrentInstance();
-         context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Exito", "Se elimino el producto "+ producto.getNombreProducto()));
-        
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Exito", "Se elimino el producto " + producto.getNombreProducto()));
+
         Inicializar();
     }
-    
-    
-    public void BUSCARPRODUCTO(int id){
+
+    public void BUSCARPRODUCTO(int id) {
         producto = pDao.BuscaporIdProducto(id);
-        if(producto.getImagen()!=null){
-            imagen =new DefaultStreamedContent(new ByteArrayInputStream(producto.getImagen()));
+        if (producto.getImagen() != null) {
+            byte[] base64Bytes = Base64.decodeBase64(producto.getImagen());
+            imagen = new DefaultStreamedContent(new ByteArrayInputStream(base64Bytes));
         }
     }
-    
-    
-    
-    
-    
+
     public List<TCategoria> getListaCategorias() {
-        listaCategorias=pDao.listarCategoria();
+        listaCategorias = pDao.listarCategoria();
         return listaCategorias;
     }
 
     public List<TProducto> getListaProductos() {
-        listaProductos=pDao.listarProductos();
+        listaProductos = pDao.listarProductos();
         return listaProductos;
     }
 
-    
-    
     public TProducto getProducto() {
         return producto;
     }
@@ -181,6 +170,5 @@ public class ProductoBean {
     public void setImagen(StreamedContent imagen) {
         this.imagen = imagen;
     }
-    
-    
+
 }
